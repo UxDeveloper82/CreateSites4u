abe2a8887ecb90035accb2ec0293d75ac629232a
UxDeveloper82 <electronicsmf@aol.com>
2020-03-28 17:02:08 -0400

    initial commit

---
+++ icreatesites4u.api/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using icreatesites4u.api.Dtos;
+using icreatesites4u.api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace icreatesites4u.api.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt => {
+                    opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+                });
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt => {
+                    opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+                });
+            CreateMap<Photo, PhotosForDetailedDto>();
+
+
+
+
+        }
+    }
+}
